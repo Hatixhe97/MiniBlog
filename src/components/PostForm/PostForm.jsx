24bf2d272604ed6a2a128bcbc0e5600286f4f414
Hatixhe97 <hatixhe.idar@gmail.com>
2025-04-05 22:55:31 +0200

    implemented PostForm UI

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,40 +1,90 @@
+import { useState } from 'react';
 import styles from './PostForm.module.css'
 
-const PostForm = () => {
+const PostForm = (props) => {
+
+  const [title,setTitle] = useState ('');
+  const [description,setDescription] = useState ('');
+  const [data,setData] = useState ('');
+  const [imageLink,setImageLink] = useState ('');
+
+  const addNewPost =()=>{
+    const newPost={
+      title,
+      description,
+      data,
+      imageLink,
+   
+  };
+
+  props.addNewPost(newPost);
+
+  setTitle ('');
+  setDescription('');
+  setData ('');
+  setImageLink ('');
+
+}
 
   return (
     <>
-    <div className={styles.container}>
-      <h1>Add New Post</h1>
 
-      <form>
+    <img className={styles.backgroundimg} src="https://everwallpaper.com/cdn/shop/products/1_beccc207-403d-4c30-a28b-1f6e337ed5f4.jpg?v=1658385121" alt="" />
+
+    <div className={styles.formconatiner}>
+      
+
+     <div className={styles.container}>
+       <h1>Add The Last Grand Adventure</h1>
+
+
+       <form>
         <div>
 
           <div className={styles.inputcontainer}>
              <label>Title</label>
-             <input type='text'/>
+             <input type='text' 
+               value={title}
+               onChange={(event) => setTitle(event.target.value)}
+              />
           </div>
          
          <div className={styles.inputcontainer}>
            <label>Description</label>
-           <textarea />
+           <textarea  type='text'
+             value={description}
+             onChange={(event) => setDescription(event.target.value)}
+            />
          </div>
 
          <div className={styles.inputcontainer}>
            <label>Date</label>
-           <input style={{width:202}} type='date'/>
+           <input style={{width:202}} type='date'
+              value={data}
+              onChange={(event) => setData(event.target.value)}
+            />
          </div>
           
           <div className={styles.inputcontainer}>
            <label>Image link</label>
-           <input type='text'/>
+           <input type='text'
+             value={imageLink}
+             onChange={(event) => setImageLink(event.target.value)}
+            />
           </div>
 
         </div>
 
-      </form>
-      <button type='submit'>Add Post</button>
+        </form>
+        <button onClick={addNewPost}>Add Post</button>
+      </div>
+
+      <div className={styles.imgcontainer}>
+        <img src="/src/assets/travel.png" alt="" />
+      </div>
     </div>
+    
+    
 
       
     </>
