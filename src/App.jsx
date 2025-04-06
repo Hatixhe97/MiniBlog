fafa0859a63de566572400cab1e738ad2da5ee7c
Hatixhe97 <hatixhe.idar@gmail.com>
2025-04-06 18:43:05 +0200

    add exemple

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import FilterBlogs from './components/FilterBlogs/FilterBlogs';
 
 function App() {
 
-  const [blogs, setBlogs]=useState([]);
+  const [blogs, setBlogs]=useState([{
+    title: 'Ticino, Switzerland',
+    description: 'This is of the most farityle – like villages on earth. This is Foroglio in Ticino, Switzerland. Located in Val Bavona, it’s only inhabited during the summer due to its lack of electricity.Without it, the colder months would be quite challenging. The village is know for its stunning 110 meter waterfalland is a serene spot to visit in spring, offering a less crowded experience compared to summer. Just 50 minute drive away lies Lavertezzo in Verzasca Valley, know for the Ponte dei Salti double – arched bridge. This medieval bridge, dating back to the 17th century, was rebuilt in 1960 after the right arch was destroyed in a flood in 1868.',
+    date: '09-04-2024',
+    imageLink: 'https://www.newlyswissed.com/wp-content/uploads/2018/05/Foroglio-Ticino-Waterfalls-01a_feature.jpg',
+  }]);
   
   const [filteredBlogs, setFilteredBlogs]=useState([]);
 
